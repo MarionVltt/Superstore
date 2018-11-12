87a5d3fde527aeaa5a7247758aaa02e665ee4948
mario <mario@LAPTOP-RMDC7G9E>
2018-11-12 17:58:02 -0500

    création projet

---
+++ src/simModel/Output.java
@@ -0,0 +1,18 @@
+package simModel;
+
+class Output 
+{
+	SMSuperstore model;
+	
+	protected Output(SMSuperstore md) { model = md; }
+    // Use OutputSequence class to define Trajectory and Sample Sequences
+    // Trajectory Sequences
+
+    // Sample Sequences
+
+    // DSOVs available in the OutputSequence objects
+    // If seperate methods required to process Trajectory or Sample
+    // Sequences - add them here
+
+    // SSOVs
+}
