afb99c65c93a173a62c7384bd55d7416cf7224dc
mario <mario@192.168.2.61>
2018-11-13 18:11:06 -0500

    manger?

--- src/simModel/Output.java
+++ src/simModel/Output.java
@@ -1,23 +1,12 @@
 package simModel;
 
-class Output 
-{
+class Output {
 	SMSuperstore model;
 	
 	protected Output(SMSuperstore md) { model = md; }
-    // Use OutputSequence class to define Trajectory and Sample Sequences
-    // Trajectory Sequences
-
-    // Sample Sequences
-
-    // DSOVs available in the OutputSequence objects
-    // If seperate methods required to process Trajectory or Sample
-    // Sequences - add them here
 
     // SSOVs
 	protected int[] numCustomers;
 	protected int[] numLongWait;
-	
-	//DSOV
 	protected double[] propLongWait;
 }
