13d3212bf3092d248620c39c8b9855dc9ebabd89
mario <mario@LAPTOP-RMDC7G9E>
2018-11-19 15:17:35 -0500

    modif baggers

--- src/simModel/Baggers.java
+++ src/simModel/Baggers.java
@@ -1,13 +1,11 @@
 package simModel;
 
-import java.util.HashSet;
 
 /*
  * Resource Group representing the baggers
  */
 public class Baggers {
-	// For implementing the group, use a HashSet object.
-	protected HashSet<Customer> group = new HashSet<Customer>();
+
 	/* nAvail value is decremented each time a bagger is assigned to a counter 
 	* and incremented when a bagger is freed from a counter. 
 	* At schedule change, the number may become negative to show that some baggers 
@@ -15,8 +13,5 @@ public class Baggers {
 	*/
 	protected int nAvail;
 	
-	// Required methods to manipulate the group
-	protected void insertGrp(Customer icCustomer) {	group.add(icCustomer); }
-	protected boolean removeGrp(Customer icCustomer) { return(group.remove(icCustomer)); }
-	protected int getN() { return group.size(); }
+	protected int getN() { return nAvail; }
 }
