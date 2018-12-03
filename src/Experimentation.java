eb25aa2d48af8d9a8a1e3d77f2703b8c7f69822a
leocr <leocr@192.168.2.24>
2018-12-03 19:26:27 -0400

    schedule optimization almost done.

--- src/Experimentation.java
+++ src/Experimentation.java
@@ -56,14 +56,94 @@ public class Experimentation {
 		int [] cashierSchedule= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 		int [] baggerSchedule= {20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20};
 		double[] propLongWait = new double[16];
+		double [] delta = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		int start, duration;
+		int run=0;
+		
 		ConfidenceInterval [] intervals = new ConfidenceInterval[16]; 
+		System.out.println("Initiating Cashier schedule optimization...");
 		intervals=runSchedule(cashierSchedule, baggerSchedule,true);
 		for(int i=0; i<16; i++) {
-			propLongWait[i]=intervals[i].getPointEstimate();
+			propLongWait[i]=intervals[i].getCfMax();
 		}
 		while(!waitOK(propLongWait,CEIL)) {
+			run +=1;
+			System.out.printf("\nRun: %d \n", run);
+			System.out.printf("Current long wait proportion: %s\n", Arrays.toString(propLongWait));
+			start = -1;
+			do{
+				start+= 1;
+			}while(start < 10 && propLongWait[start]<CEIL) ;
+			if(start>0) {
+				start -=1;
+			}
+			duration=5;
+			do {
+				duration +=1;
+			}while(duration < 10 && start+duration < 16 && propLongWait[start+duration-1]>=CEIL);
+			for(int i=0; i<16; i++) {
+				delta[i]=0;
+			}
+			for(int i=0; i<duration; i++) {
+				delta[start+i]+=1;
+			}
+			System.out.printf("Proposed modification: %s \n", Arrays.toString(delta));
+			for(int i=0; i<16; i++) {
+				cashierSchedule[i]+=delta[i];
+			}
+			System.out.printf("New Cashier schedule: %s \n", Arrays.toString(cashierSchedule));
+			intervals = runSchedule(cashierSchedule,baggerSchedule,false);
+			for(int i=0; i<16; i++) {
+				propLongWait[i]=intervals[i].getCfMax();
+			}
 			
 		}
+		System.out.printf("\n\n\nOptimal Cashier Schedule: %s\n", Arrays.toString(cashierSchedule));
+		System.out.printf("Proportion of long wait times: %s\n", Arrays.toString(propLongWait));
+		System.out.println("Cashier schedule optimization done.\nBagger schedule optimization begining...");
+		for(int i=0; i<16; i++) {
+			baggerSchedule[i]=0;
+		}
+		
+		run +=1;
+		intervals=runSchedule(cashierSchedule, baggerSchedule,false);
+		for(int i=0; i<16; i++) {
+			propLongWait[i]=intervals[i].getCfMax();
+		}
+		while(!waitOK(propLongWait,CEIL)) {
+			run +=1;
+			System.out.printf("\nRun: %d \n", run);
+			System.out.printf("Current long wait proportion: %s\n", Arrays.toString(propLongWait));
+			start = -1;
+			do{
+				start+= 1;
+			}while(start < 10 && propLongWait[start]<CEIL) ;
+			if(start>0) {
+				start -=1;
+			}
+			duration=5;
+			do {
+				duration +=1;
+			}while(duration < 10 && start+duration < 16 && propLongWait[start+duration-1]>=CEIL);
+			for(int i=0; i<16; i++) {
+				delta[i]=0;
+			}
+			for(int i=0; i<duration; i++) {
+				delta[start+i]+=1;
+			}
+			System.out.printf("Proposed modification: %s \n", Arrays.toString(delta));
+			for(int i=0; i<16; i++) {
+				baggerSchedule[i]+=delta[i];
+			}
+			System.out.printf("New bagger schedule: %s \n", Arrays.toString(baggerSchedule));
+			intervals = runSchedule(cashierSchedule,baggerSchedule,false);
+			for(int i=0; i<16; i++) {
+				propLongWait[i]=intervals[i].getCfMax();
+			}
+			
+		}
+		System.out.printf("\n\n\nOptimal bagger Schedule: %s\n", Arrays.toString(baggerSchedule));
+		System.out.printf("Proportion of long wait times: %s\n", Arrays.toString(propLongWait));
 		
 		
 		
