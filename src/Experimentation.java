a38df2d1b1d9b20bb993a5ecadc8bac4fc37d4ea
leocr <leocr@192.168.2.24>
2018-12-05 15:20:12 -0400

    small optimization to schedule algorithm, and comment of the whole code.

--- src/Experimentation.java
+++ src/Experimentation.java
@@ -8,13 +8,13 @@ import java.util.ArrayList;
 
 public class Experimentation {
 
-	static final int NUMRUNS = 1000;
-	static final double confidence = 0.90;
-	static final double CEIL = 0.10;
-	static final double startTime=0.0, endTime=480;
-	static RandomSeedGenerator rsg = new RandomSeedGenerator();
+	static final int NUMRUNS = 1000; //number of runs per experiment
+	static final double confidence = 0.90; //confidence level
+	static final double CEIL = 0.10; // maximum proportion of customer waiting more than 15 min allowed
+	static final double startTime=0.0, endTime=480; //simulation start and end times.
+	static RandomSeedGenerator rsg = new RandomSeedGenerator(); //random seed generator
 
-	static public boolean waitOK(double [] propLongWait, double ceil) {
+	static public boolean waitOK(double [] propLongWait, double ceil) {// verifies that for each period the proportion of customers waiting more than 15 is below CEIL
 		for(int i = 0; i<16; i++) {
 			if(propLongWait[i]>ceil)
 				return false;
@@ -22,15 +22,20 @@ public class Experimentation {
 		return true;
 	}
 
+
+
+
+
+	//This function executes an experiment with given schedule, by running the simulation 1000 times, and returning the CI for each period.
 	static public ConfidenceInterval[] runSchedule(int [] cashierSchedule, int []baggerSchedule,Seeds[] seeds, boolean verbose) {
-		SMSuperstore store;
+		SMSuperstore model; // instantiate model
 		ConfidenceInterval [] intervals = new ConfidenceInterval[16];
-		double [][] values = new double[16][NUMRUNS];
+		double [][] values = new double[16][NUMRUNS]; 
 
 		for(int i = 0; i<NUMRUNS; i++) {
-			store = new SMSuperstore(startTime, endTime,cashierSchedule,baggerSchedule,seeds[i],false);
-			store.runSimulation();
-			double [] results = store.getPropLongWait();
+			model = new SMSuperstore(startTime, endTime,cashierSchedule,baggerSchedule,seeds[i],false);
+			model.runSimulation();
+			double [] results = model.getPropLongWait();
 			for(int j = 0; j<16; j++) {
 				values[j][i] = results[j];
 			}
@@ -39,6 +44,8 @@ public class Experimentation {
 		for(int j =0; j< 16; j++) {
 			intervals[j]=new ConfidenceInterval(values[j],confidence);
 		}
+
+		// debugging print enable by parameter verbose
 		if(verbose) {
 			System.out.printf("-------------------------------------------------------------------------------------\n");
 			System.out.printf("Comparison    Point estimate(ybar(n))  s(n)     zeta   CI Min   CI Max |zeta/ybar(n)|\n");
@@ -54,59 +61,73 @@ public class Experimentation {
 		return intervals;
 	}
 
+
+
+
+
 	public static void main(String[] args) {
-		int [] cashierSchedule= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-		int [] baggerSchedule= {20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20};
-		double[] propLongWait = new double[16];
-		double [] delta = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-		int start, duration;
-		int run=0;
-		boolean backtrackFail=false;
-		Seeds [] seeds = new Seeds[NUMRUNS];
+		int [] cashierSchedule= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; //cashiers schedule
+		int [] baggerSchedule= {20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20}; //bagger schedule, initially set to 20 per period to eliminate bagger bottleneck.
+		double[] propLongWait = new double[16]; //proportion of customer that wait more than 15 minutes at each period.
+		double [] delta = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //number of employee delta for each period, only useful for printing/debuging purposes.
+		int shiftStart, shiftDuration; //used to compute the period at which the newly added employee will start and how many periods he will work.
+		int run=0; //number of simulation runs
+		Seeds [] seeds = new Seeds[NUMRUNS]; //seeds for the 1000 iteration of each run. All the runs will use the same 1000 seeds to reduce variance.
+		/*
+		 * Uncomment this block to get different seeds when launching the schedule optimization.
+		 * 
 		int randomSeed=(int) System.currentTimeMillis()%10;
-		for(int i=0; i<randomSeed; i++) {
+ 		for(int i=0; i<randomSeed; i++) {
 			for(int j=0; j<NUMRUNS;j++) {
 				rsg.nextSeed();
 			}
 		}
-		for(int i=0; i<NUMRUNS;i++) {
+
+
+		 */
+		for(int i=0; i<NUMRUNS;i++) {//generate the 1000 seeds.
 			seeds[i]=new Seeds(rsg);
 		}
-		ConfidenceInterval [] intervals = new ConfidenceInterval[16]; 
+
+		ConfidenceInterval [] intervals = new ConfidenceInterval[16]; //16 confidence interval, one for each period.
 		System.out.println("Initiating Cashier schedule optimization...");
-		intervals=runSchedule(cashierSchedule, baggerSchedule, seeds, true);
+		intervals=runSchedule(cashierSchedule, baggerSchedule, seeds, true); //get base line number.
 		for(int i=0; i<16; i++) {
-			propLongWait[i]=intervals[i].getCfMax();
+			propLongWait[i]=intervals[i].getCfMax();//use CfMax instead of point estimate so that the actual value is 90% certain to be below CEIL
 		}
-		while(!waitOK(propLongWait,CEIL)) {
-			run +=1;
+
+		//This is the cashier schedule loop.
+		while(!waitOK(propLongWait,CEIL)) { //have we met our objective?
+			run +=1; // new experiment going on
 			System.out.printf("\nRun: %d \n", run);
 			System.out.printf("Current long wait proportion: %s\n", Arrays.toString(propLongWait));
-			start = -1;
+			shiftStart = -1; // initialized to -1 since it will be increased in the do..while
 			do{
-				start+= 1;
-			}while(start < 10 && propLongWait[start]<CEIL) ;
-			if(start>0) {
-				start -=1;
+				shiftStart+= 1;
+			}while(shiftStart < 11 && propLongWait[shiftStart]<CEIL) ; //find first period where customers wait too much, and use it as start of shift for next cashier.
+			// Cannot start a shift after 11 otherwise the 
+			if(shiftStart>0) {
+				shiftStart -=1; //The accumulation of customers in the previous period affects the results of he current period, 
+				//so make the employee start one period earlier.
 			}
-			duration=5;
+			shiftDuration=5; // initialized to 5 since it is going to be incremented to 6 in the do..while. Start with shift duration equal to 6 so that employee works at least 3 hours.
 			do {
-				duration +=1;
-			}while(duration < 10 && start+duration < 16 && propLongWait[start+duration-1]>=CEIL);
+				shiftDuration +=1;
+			}while(shiftDuration < 10 && shiftStart+shiftDuration < 16 && propLongWait[shiftStart+shiftDuration-1]>=CEIL);//Shift duration should be less or equal to 5 hours, and end of shift should be at most the last period.
 			for(int i=0; i<16; i++) {
 				delta[i]=0;
 			}
-			for(int i=0; i<duration; i++) {
-				delta[start+i]+=1;
+			for(int i=0; i<shiftDuration; i++) {
+				delta[shiftStart+i]+=1; //add one employee between shift start and shift start+duration
 			}
 			System.out.printf("Proposed modification: %s \n", Arrays.toString(delta));
 			for(int i=0; i<16; i++) {
-				cashierSchedule[i]+=delta[i];
+				cashierSchedule[i]+=delta[i]; //apply schedule modification
 			}
 			System.out.printf("New Cashier schedule: %s \n", Arrays.toString(cashierSchedule));
-			intervals = runSchedule(cashierSchedule,baggerSchedule, seeds, false);
+			intervals = runSchedule(cashierSchedule,baggerSchedule, seeds, false); //run new experiment with new schedule
 			for(int i=0; i<16; i++) {
-				propLongWait[i]=intervals[i].getCfMax();
+				propLongWait[i]=intervals[i].getCfMax(); //update propLongWait
 			}
 
 		}
@@ -114,101 +135,119 @@ public class Experimentation {
 		System.out.printf("Proportion of long wait times: %s\n", Arrays.toString(propLongWait));
 		System.out.println("Cashier schedule optimization done.\nBagger schedule optimization begining...");
 		for(int i=0; i<16; i++) {
-			baggerSchedule[i]=1 ;
+			baggerSchedule[i]=0 ; // reset bagger schedule to begin optimization
 		}
 
 		run +=1;
-		intervals=runSchedule(cashierSchedule, baggerSchedule, seeds, false);
-		ArrayList<Integer> backtrackingStart = new ArrayList<Integer>();
-		ArrayList<Integer> backtrackingDuration = new ArrayList<Integer>();
+		intervals=runSchedule(cashierSchedule, baggerSchedule, seeds, false); // get baseline results.
+		
 		for(int i=0; i<16; i++) {
 			propLongWait[i]=intervals[i].getCfMax();
 		}
-		while(!waitOK(propLongWait,CEIL)) {
-			run +=1;
+		
+		ArrayList<Integer> backtrackingStart = new ArrayList<Integer>(); // Arraylist used to keep track of the baggers shift start as we add them, so that we can undo our modifications if need be.
+		ArrayList<Integer> backtrackingDuration = new ArrayList<Integer>();// Arraylist used to keep track of the baggers shift duration as we add them, so that we can undo our modifications if need be.
+		
+		
+		//bagger main loop, see comments of cashier loop for more details as they are amlost the same.
+		while(!waitOK(propLongWait,CEIL)) { //have we met our objective?
+			run +=1; // new experiment done here
 			System.out.printf("\nRun: %d \n", run);
 			System.out.printf("Current long wait proportion: %s\n", Arrays.toString(propLongWait));
-			start = -1;
+			shiftStart = -1; // 
 			do{
-				start+= 1;
-			}while(start < 10 && propLongWait[start]<CEIL) ;
-			if(start>0) {
-				start -= 1;
+				shiftStart+= 1;
+			}while(shiftStart < 11 && propLongWait[shiftStart]<CEIL) ; // find bagger shift start according to schedule constraints
+			if(shiftStart>0) {
+				shiftStart -= 1; //previous period as a lot of impact on current period, so start a period earlier.
 			}
-			duration=5;
+			shiftDuration=5;
 			do {
-				duration +=1;
-			}while(duration < 10 && start+duration < 16 && propLongWait[start+duration-1]>=CEIL);
+				shiftDuration +=1;
+			}while(shiftDuration < 10 && shiftStart+shiftDuration < 16 && propLongWait[shiftStart+shiftDuration-1]>=CEIL); //find shift duration according to schedule constraints.
 
-			backtrackingStart.add(start);
-			backtrackingDuration.add(duration);
+			backtrackingStart.add(shiftStart); //add shift start to employee history
+			backtrackingDuration.add(shiftDuration);// add shift duration employee history
 
 			for(int i=0; i<16; i++) {
 				delta[i]=0;
 			}
-			for(int i=0; i<duration; i++) {
-				delta[start+i]+=1;
+			for(int i=0; i<shiftDuration; i++) {
+				delta[shiftStart+i]+=1; //add employee to schedule
 			}
 
 			for(int i=0; i<16; i++) {
-				baggerSchedule[i]+=delta[i];
+				baggerSchedule[i]+=delta[i]; //make modifications effective
 			}
 
-			int problemStart=0;
+			// verify that there is no schedule problems
+			int problemPeriod=0; // holds the index of the problematic period.
 			do {
-				problemStart++;
-			}while(problemStart<16 && baggerSchedule[problemStart]<=cashierSchedule[problemStart]);
+				problemPeriod++;
+			}while(problemPeriod<16 && baggerSchedule[problemPeriod]<=cashierSchedule[problemPeriod]); // scan each period, and verify that there aren't more baggers than cashiers
 
-			System.out.printf("Proposed bagger schedule: %s \n", Arrays.toString(baggerSchedule));
-			if (! backtrackFail && problemStart<16) {
-				System.out.printf("original  problmeStart: %d\n", problemStart);
-				while(problemStart>0 && cashierSchedule[problemStart-1]==baggerSchedule[problemStart-1]) {
-					problemStart--;
+			/* 
+			 * In this block we will apply a backtracking algorithm to fix cases where our simple algorithm tries to add more baggers
+			 * than there are cashiers in a given period. This can happen because cashiers have less of an influence on the queue times
+			 * compared to cashiers, and so interdependence between periods is more important when optimizing the bagger's schedule.
+			 * The basic working principle of this algorithm is as follows:
+			 *   * find the first period where there are more baggers than cashiers
+			 *   * find the first period P1 where the number of baggers is equal to the number of baggers 
+			 *   * using the history of the employees, remove all employees with shifts starting at and after P1
+			 *   * add a new employee starting at period P1-1, and continue the optimization.
+			 */
+			if (problemPeriod<16) {
+				
+				for(int i=0; i<16; i++) { // clear delta so we can use it to see the backtracking modifications
+					delta[i]=0;
 				}
-				if(problemStart==0) {
-					backtrackFail=true;
-					System.out.println("backtrack failed, defaulting to cashier schedule");
-					for(int i=0; i<16; i++) {
-						baggerSchedule[i]=cashierSchedule[i];
-					}
+				System.out.printf("Proposed bagger schedule: %s \n", Arrays.toString(baggerSchedule));
+				System.out.printf("Current cashier schedule: %s \n", Arrays.toString(cashierSchedule));
+				System.out.printf("original  problmeStart: %d\n", problemPeriod);
+				while(problemPeriod>0 && cashierSchedule[problemPeriod-1]==baggerSchedule[problemPeriod-1]) { //find first period where there is room for a new employee
+					problemPeriod--;
 				}
-				else {
-					System.out.printf("updated problmeStart: %d\n", problemStart);
-					System.out.println("backtracking start" + Arrays.toString(backtrackingStart.toArray()));
-					System.out.println("backtracking duration" + Arrays.toString(backtrackingDuration.toArray()));
-					int beginning = 0;
-					for(int i=0; i<backtrackingStart.size(); i++) {
-						if(backtrackingStart.get(i)>=problemStart) {
-							if(beginning==0) beginning=i;
-							for(int j=0; j<backtrackingDuration.get(i); j++) {
-								baggerSchedule[problemStart+j] --;
-							}
+				System.out.printf("updated problmeStart: %d\n", problemPeriod);
+				System.out.println("Shift start history before backtracking: " + Arrays.toString(backtrackingStart.toArray()));
+				System.out.println("Shift duration history before backtracking: " + Arrays.toString(backtrackingDuration.toArray()));
+				int beginning = -1; //this variable will hold the index of the first "problematic" employee in the history of employees 
+				for(int i=0; i<backtrackingStart.size(); i++) { //scan history of employees
+					if(backtrackingStart.get(i)>=problemPeriod) { //employee that started later than the problematic period
+						if(beginning==-1) beginning=i; //if it's the first of such employee, update beginning 
+						for(int j=0; j<backtrackingDuration.get(i); j++) { //remove the employee from the schedule.
+							baggerSchedule[problemPeriod+j] --;
+							delta[problemPeriod+j]--;
 						}
 					}
-					int newDuration = backtrackingDuration.get(beginning);
-					for(int i=0; i<newDuration; i++) {
-						baggerSchedule[problemStart-1+i] ++;
-					}
-
-					int nbToRemove = backtrackingStart.size()-beginning;
-					for(int i=nbToRemove; i>0; i--) {
-						backtrackingStart.remove(beginning+i-1);
-						backtrackingDuration.remove(beginning+i-1);
-					}
-					backtrackingStart.add(problemStart-1);
-					backtrackingDuration.add(newDuration);
-					System.out.println("backtracking start" + backtrackingStart);
-					System.out.println("backtracking duration" + backtrackingDuration);
+				}//at this stage we have removed all the employees starting later than the problematic period
+				
+				
+				int newDuration = backtrackingDuration.get(beginning); //new employee gets same duration than the employee it's replacing
+				for(int i=0; i<newDuration; i++) {
+					baggerSchedule[problemPeriod-1+i] ++;//add new employee to schedule, starting at period before problematic period.
+					delta[problemPeriod-1+i]++;
 				}
+
+				int nbToRemove = backtrackingStart.size()-beginning; // number of employees removed during back tracking phase
+				for(int i=nbToRemove; i>0; i--) {
+					backtrackingStart.remove(beginning+i-1);//clear history
+					backtrackingDuration.remove(beginning+i-1);
+				}
+				backtrackingStart.add(problemPeriod-1);//add new employee to history
+				backtrackingDuration.add(newDuration);
+				
+				// at this stage the backtracking is over, the optimization process can continue normally
+				System.out.printf("backtracking modification to the schedule: %s\n", Arrays.toString(delta));
+				System.out.println("Shift start history after backtracking: " + backtrackingStart);
+				System.out.println("Shift duration history after backtracking: " + backtrackingDuration);
 			}
 			System.out.printf("Proposed modification: %s \n", Arrays.toString(delta));
 			System.out.printf("Cashier schedule: %s\n", Arrays.toString(cashierSchedule));
 			System.out.printf("New bagger schedule: %s \n", Arrays.toString(baggerSchedule));
-			intervals = runSchedule(cashierSchedule,baggerSchedule, seeds, false);
+			intervals = runSchedule(cashierSchedule,baggerSchedule, seeds, false); //run new experiment with updated schedule.
 			for(int i=0; i<16; i++) {
 				propLongWait[i]=intervals[i].getCfMax();
 			}
-
 		}
 		System.out.printf("\n\n\nOptimal bagger Schedule: %s\n", Arrays.toString(baggerSchedule));
 		System.out.printf("Proportion of long wait times: %s\n", Arrays.toString(propLongWait));
